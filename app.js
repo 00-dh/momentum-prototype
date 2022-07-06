df0e8a0a9b8c7b8b538c7038276250622dd97842
00-dh <kimdh081599@gmail.com>
2022-07-06 15:44:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const amIFat = null;
-let something;
-console.log(something);
+const dayOfWeek = ["mon", "tue", "wen", "thu", "fri", "sat", "sun"];
+
+// Get Item From Array
+console.log(dayOfWeek);
+
+// Add Item to the Array
+dayOfWeek.push("sun");
+
+console.log(dayOfWeek);
