17a7eecdf029879f39e45b6463ff785d42219349
00-dh <kimdh081599@gmail.com>
2022-07-07 16:06:48 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const player = {
-  name: "nico",
-  age: 98,
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  powerOf: function (a, b) {
+    console.log(a ** b);
+  },
 };
 
-player.name = "DH";
-player.sexy = "soon";
-
-console.log(player);
+calculator.add(5, 2);
+calculator.minus(5, 2);
+calculator.divide(5, 2);
+calculator.powerOf(5, 2);
