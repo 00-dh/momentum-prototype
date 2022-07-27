47c4a1dcf6644bde30c3d594a9aca0da99a3470a
00-dh <kimdh081599@gmail.com>
2022-07-27 16:46:14 +0900

    learn about toggle in js

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const h1 = document.querySelector("#hello h1");
 
 function handleMouseEnter() {
   const clickedClassName = "active";
-  if (h1.classList.contains(clickedClassName)) {
-    h1.classList.remove(clickedClassName);
-  } else {
-    h1.classList.add(clickedClassName);
-  }
+  h1.classList.toggle(clickedClassName);
 }
 
 h1.addEventListener("click", handleMouseEnter);
