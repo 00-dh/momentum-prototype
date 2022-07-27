b714a2f372fd8a4fe79471bec0e7eb341aa0fcba
00-dh <kimdh081599@gmail.com>
2022-07-27 21:21:55 +0900

    learn about localStorage deep

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 const greeting = document.querySelector("#greeting");
 
-function submitForm(event) {
-  const hidden_className = "hidden";
+const USERNAME_KEY = "username";
+const hidden_className = "hidden";
 
+function submitForm(event) {
   event.preventDefault();
   loginForm.classList.add(hidden_className);
   const userName = loginInput.value;
-  localStorage.setItem("username", userName);
-  console.log(userName);
+  localStorage.setItem(USERNAME_KEY, userName);
   greeting.innerText = `hello ${userName}`;
   greeting.classList.remove(hidden_className);
 }
 
+function paintGreeting(username) {
+  greeting.innerText = `hello ${username}`;
+  greeting.classList.remove(hidden_className);
+}
+
 loginForm.addEventListener("submit", submitForm);
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+console.log(savedUserName);
+
+if (savedUserName === null) {
+  loginForm.classList.remove(hidden_className);
+} else {
+  paintGreeting(savedUserName);
+}
