d0b55c79c89ba068a533bd1b9f5a534521348d05
00-dh <kimdh081599@gmail.com>
2022-07-28 20:11:59 +0900

    learn about make clock

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,11 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  const Hours = String(date.getHours()).padStart(2, "0");
+  const Minutes = String(date.getMinutes()).padStart(2, "0");
+  const Seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${Hours}:${Minutes}:${Seconds}`;
 }
-
-setTimeout(sayHello, 5000);
+getClock();
+setInterval(getClock, 1000);
