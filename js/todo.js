5268b8289cdde059a6bac2365608dbd8ab0f2ca7
00-dh <kimdh081599@gmail.com>
2022-07-30 18:12:19 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,28 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDO(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
-  const newToDo = toDoInput.value;
+  const newTodo = toDoInput.value;
   toDoInput.value = "";
+  paintToDO(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
